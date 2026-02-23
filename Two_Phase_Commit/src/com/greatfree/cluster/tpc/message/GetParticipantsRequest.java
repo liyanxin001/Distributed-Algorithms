3f93531d5cc3948c3245917c81448b69e99e3223
apple <apple@localhost>
2026-02-23 20:59:01 +0800

    1

--- Two_Phase_Commit/src/com/greatfree/cluster/tpc/message/GetParticipantsRequest.java
+++ Two_Phase_Commit/src/com/greatfree/cluster/tpc/message/GetParticipantsRequest.java
@@ -1,12 +1,29 @@
 package com.greatfree.cluster.tpc.message;
 
+
+
 import edu.greatfree.cluster.message.ClusterRequest;
+import edu.greatfree.cluster.message.RequestType;
 
 public class GetParticipantsRequest extends ClusterRequest {
 
-	public GetParticipantsRequest(int appID) {
-		super(appID);
-		// TODO Auto-generated constructor stub
+	private static final long serialVersionUID = -1759133752348447207L;
+	
+	
+	private String transactionId;
+
+	public GetParticipantsRequest(String transactionId) {
+		super(RequestType.BROADCAST_REQUEST);
+		this.setTransactionId(transactionId);
+
+	}
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
 	}
 	
 
