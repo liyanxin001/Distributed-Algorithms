29d90ad273dcb283a20dafe6415a85097cc8bd91
liyanxin001 <liyanxin003@126.com>
2026-02-24 22:28:58 +0800

    5

--- Two_Phase_Commit/src/com/greatfree/cluster/tpc/child/app/BankParticipant.java
+++ Two_Phase_Commit/src/com/greatfree/cluster/tpc/child/app/BankParticipant.java
@@ -3,6 +3,8 @@ package com.greatfree.cluster.tpc.child.app;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.greatfree.cluster.tpc.data.Account;
+
 /**
  * Represents the possible states of a distributed transaction within a participant.
  * This follows the standard 2PC state transition:
@@ -19,41 +21,7 @@ enum TransactionState {
 /**
  * Simple bank account entity.
  */
-class Account {
-    private final String accountNumber;
-    private double balance;
-    
-    public Account(String accountNumber, double initialBalance) {
-        this.accountNumber = accountNumber;
-        this.balance = initialBalance;
-    }
-    
-    public String getAccountNumber() { return accountNumber; }
-    public double getBalance() { return balance; }
-    
-    public void withdraw(double amount) {
-        if (balance < amount) {
-            throw new InsufficientFundsException(
-                String.format("Insufficient funds: have $%.2f, need $%.2f", balance, amount));
-        }
-        balance -= amount;
-    }
-    
-    public void deposit(double amount) {
-        balance += amount;
-    }
-    
-    @Override
-    public String toString() {
-        return String.format("%s: $%.2f", accountNumber, balance);
-    }
-}
 
-class InsufficientFundsException extends RuntimeException {
-    private static final long serialVersionUID = 2363070223116303575L;
-
-	public InsufficientFundsException(String message) { super(message); }
-}
 
 /**
  * Represents a lock held on an account by a transaction.
