6ab843972c8bcfbf513e3f6e39d3828711e53821
liyanxin001 <liyanxin003@126.com>
2026-04-04 21:45:36 +0800

    1

---
+++ LCR/src/com/greatfree/ring/lcr/process/ProcessClient.java
@@ -0,0 +1,23 @@
+package com.greatfree.ring.lcr.process;
+
+import org.greatfree.client.FreeClientPool;
+import org.greatfree.client.SyncRemoteEventer;
+import org.greatfree.message.ServerMessage;
+
+final class ProcessClient 
+{
+	private SyncRemoteEventer<ServerMessage> eventer;
+	
+	public ProcessClient(FreeClientPool clientPool) {
+		this.setEventer(new SyncRemoteEventer<ServerMessage>(clientPool));
+	}
+
+	public SyncRemoteEventer<ServerMessage> getEventer() {
+		return eventer;
+	}
+
+	public void setEventer(SyncRemoteEventer<ServerMessage> eventer) {
+		this.eventer = eventer;
+	}
+
+}
