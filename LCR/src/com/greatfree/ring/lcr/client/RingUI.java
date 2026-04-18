7eb31d9a3c4459aadad7475f92ab71dc93d1f639
liyanxin001 <liyanxin003@126.com>
2026-04-18 20:52:28 +0800

    1

---
+++ LCR/src/com/greatfree/ring/lcr/client/RingUI.java
@@ -0,0 +1,62 @@
+package com.greatfree.ring.lcr.client;
+
+import java.io.IOException;
+
+import org.greatfree.exceptions.RemoteReadException;
+
+
+final class RingUI {
+	
+	private String ip;
+	private int port;
+	
+	
+    private RingUI() {
+    	
+    }
+	private static RingUI instance = new RingUI(); 
+	
+	public static RingUI R()
+	{
+		if(instance == null) 
+		{
+			instance = new RingUI();
+			return instance;
+		}
+		else
+		{
+			return instance;	
+		}
+	
+	}
+	
+	public void init() throws ClassNotFoundException, RemoteReadException, IOException {
+		this.setIp("192.168.1.25");
+		this.setPort(8001);
+	}
+	
+	
+	public void printMenu() {
+		System.out.println("Start leader election? Y/N");
+	}
+	
+	public void execute() {
+		
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+}
