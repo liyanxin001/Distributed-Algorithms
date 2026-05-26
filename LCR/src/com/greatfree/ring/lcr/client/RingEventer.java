f933548f680daafdf4b30c66c8ef411f014914f0
liyanxin001 <liyanxin003@126.com>
2026-05-26 22:20:22 +0800

    1

--- LCR/src/com/greatfree/ring/lcr/client/RingEventer.java
+++ LCR/src/com/greatfree/ring/lcr/client/RingEventer.java
@@ -1,5 +1,35 @@
 package com.greatfree.ring.lcr.client;
 
-public class RingEventer {
+import org.greatfree.client.FreeClientPool;
+import org.greatfree.client.SyncRemoteEventer;
 
+import com.greatfree.ring.lcr.message.SendNotification;
+
+final class RingEventer 
+{
+	
+	private SyncRemoteEventer<SendNotification> eventer;
+	private FreeClientPool clientPool;
+	private static RingEventer instance = new RingEventer();
+	
+	
+	public static RingEventer RE() {
+		if(instance == null) {
+			
+			instance = new RingEventer();
+			return instance;
+		}
+		
+		
+		return instance;
+	}
+	
+	public void dispose() {
+		this.eventer.dispose();
+		this.clientPool.dispose();
+	}
+
+	public void init() {
+		this.clientPool = new FreeClientPool(10);
+	}
 }
