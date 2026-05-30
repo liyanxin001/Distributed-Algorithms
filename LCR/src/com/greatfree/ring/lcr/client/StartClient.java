10192418735aed9c243cc7dad1e8667d1716cdb5
liyanxin001 <liyanxin003@126.com>
2026-05-30 22:30:58 +0800

    1

--- LCR/src/com/greatfree/ring/lcr/client/StartClient.java
+++ LCR/src/com/greatfree/ring/lcr/client/StartClient.java
@@ -3,16 +3,30 @@ package com.greatfree.ring.lcr.client;
 import java.io.IOException;
 
 import org.greatfree.exceptions.RemoteReadException;
+import org.greatfree.util.Tools;
+
+
 
 final class StartClient {
 	
-	public static void main(String[] args) throws ClassNotFoundException, RemoteReadException, IOException {
+	public static void main(String[] args) throws ClassNotFoundException, RemoteReadException, IOException, InterruptedException {
 	
 		
         RingEventer.RE().init();
         RingUI.R().init();
         
 	    RingUI.R().printMenu();
+	    
+	    String optionStr;
+		int option = -1;
+		
+		optionStr = Tools.INPUT.nextLine();
+		option = Integer.parseInt(optionStr);
+		
+		while(option != 1) {
+			RingUI.R().execute();
+		}
+	    
 	}
 	
 
